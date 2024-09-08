fc408af78e83f323f2a1e4aa5fc6dd1135d4b2c5
Arathi <avallipuranathan@gmail.com>
2024-09-08 14:35:49 -0400

    Update 567. Permutation in String.py

--- 567. Permutation in String.py
+++ 567. Permutation in String.py
@@ -16,38 +16,39 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-
-        if len(s1) > len(s2): return False
+        
+        if len(s1) > len(s2): return False 
 
         s1Count, s2Count = [0] * 26, [0] * 26
         matches = 0
         l = 0
-        
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
-        
-        for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
 
+        for i in range(len(s1)):  # populate occurances
+            s1Count[ord(s1[i]) - ord('a')] += 1 
+            s2Count[ord(s2[i]) - ord('a')] += 1 
+        
+        for i in range(26):  # check matches
+            matches += 1 if s1Count[i] == s2Count[i] else 0
+        
         for r in range(len(s1), len(s2)):
             if matches == 26: return True
 
+            # check new right element added
             i = ord(s2[r]) - ord('a')
             s2Count[i] += 1
-            if s1Count[i] == s2Count[i]:
+            if s2Count[i] == s1Count[i]:
                 matches += 1
-            elif s1Count[i] == s2Count[i] - 1:
+            elif s2Count[i] - 1 == s1Count[i]:  # matching before incr
                 matches -= 1
 
+            # check old left element to remove
             i = ord(s2[l]) - ord('a')
             s2Count[i] -= 1
-            if s1Count[i] == s2Count[i]:
+            if s2Count[i] == s1Count[i]:
                 matches += 1
-            elif s1Count[i] == s2Count[i] + 1:
+            elif s2Count[i] + 1 == s1Count[i]:  # matching before decr
                 matches -= 1
             
             l += 1
-        
+
         return matches == 26
-        
