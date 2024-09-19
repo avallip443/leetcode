17bd8658fabe631b02e88f501d9ae7aa0ed2dc16
Arathi <avallipuranathan@gmail.com>
2024-09-19 10:45:10 -0400

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,12 +1,8 @@
 """
-Brute: Use nested loops to add every pair to check if their sum is the target.
-- Time complexity: O(n^2)
+Solution: Use hashmap to store visited values/indexes and check if target-current in hash.
 
-Optimized: Use a hashmap to map visited values and their indexes.
-- Iterate over list and check if target - current value = value in hashmap.
 Time complexity: O(n)
 Space complexity: O(n)
-
 """
 
 class Solution(object):
@@ -16,13 +12,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-
-        hash = {}
-
-        for i, num in enumerate(nums):
-            target_summand = target - num
-
-            if target_summand in hash:
-                return [hash[target_summand], i]
-            hash[num] = i
         
+        visited = {}
+
+        for i, t in enumerate(nums):
+            diff = target - t
+            
+            if diff in visited:
+                return [i, visited[diff]]
+            visited[t] = i
+    
