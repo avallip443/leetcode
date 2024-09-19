2b6043e4f08e356aaec6b1de94d2a684dc2a7622
Arathi <avallipuranathan@gmail.com>
2024-09-19 10:42:18 -0400

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,9 +1,6 @@
 """
-Solution: Both are anagrams if they have the same length, characters, and quantity of each character.
-- Check if strings are the same length
-- Use two hash maps to map characters to its occurances in each string
-- Iterate through one hash map's keys and compare it to the other's values
-- True if all values are the same
+Solution: Both are anagrams if they have the same length, characters, and quantity of each character. Use hashmaps to count occurances and compare if their counts are the same.
+
 Time complexity: O(s + t)
 Space complexity: O(s + t)
 """
@@ -16,18 +13,20 @@ class Solution(object):
         :rtype: bool
         """
 
-        if len(s) != len(t):
-            return False
-
-
-        hash_s, hash_t = {}, {}
-
-        for i in range(len(s)):
-            hash_s[s[i]] = 1 + hash_s.get(s[i], 0)
-            hash_t[t[i]] = 1 + hash_t.get(t[i], 0)
+        if len(s) != len(t): return False
         
-        for i in hash_s:
-            if hash_s.get(i) != hash_t.get(i, 0):
-                return False
+        hashS = {}
+        hashT = {}
 
+        # count occurances of letters
+        for i in range(len(s)):
+            hashS[s[i]] = 1 + hashS.get(s[i], 0)
+            hashT[t[i]] = 1 + hashT.get(t[i], 0)
+
+        # check if they have same keys and values
+        for k in hashS:
+            if hashS.get(k) != hashT.get(k):
+                return False
+            
         return True
+        
