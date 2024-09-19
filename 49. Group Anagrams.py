fbbedd10680cd946473ee7cc5a16e77f46cccae1
Arathi <avallipuranathan@gmail.com>
2024-09-19 11:07:34 -0400

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,8 +1,5 @@
 """
-Solution: Strings are anagrams if they have the same characters and quantity for each character.
-- Use a hashmap to map the quantity and combination of characters to a list of strings that match it
-- Key: array of len 26 (for each letter) where each index represents the number of each character
-- Index of chars will be determined by subtracing the ASCII value of the char from the ASCII value of 'a'
+Solution: Strings are anagrams if they have the same characters and counts. Use hashmap (with values as lists) to group words with the same letter/counts (use array of 26 for anagram signature).
 Eg. key [1, 1, 1] will map to list of strings like 'abc', 'bca', etc
 
 Time complexity: O(m * n) - m = # of given strings, n = avg len of str
@@ -16,15 +13,15 @@ class Solution(object):
         :rtype: List[List[str]]
         """
 
-        my_hash = defaultdict(list)
+        groups = defaultdict(list)
+        res = []
 
         for s in strs:
-            countChars = [0] * 26  # each index for each letter
+            signature = [0] * 26  # idx for each letter
+            for c in s:
+                signature[ord(c) - ord('a')] += 1
 
-            for char in s:
-                countChars[ord(char) - ord("a")] += 1  # add for each letter occurance
-            
-            my_hash[tuple(countChars)].append(s)
-
-        return my_hash.values()
+            groups[tuple(signature)].append(s)
+        
+        return groups.values()
         
